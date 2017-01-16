15a1247c67175ef79c08fe71a2b280909478e730
Jake Wiesler <jakewiesler@gmail.com>
2017-01-16 17:53:38 -0500

    Separate babel configuration out to .babelrc file

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,7 @@
  * Requires
  */
 const HtmlWebpackPlugin = require('html-webpack-plugin'),
-                   path = require('path');
-
+  path = require('path');
 
 /**
  * Variables
@@ -13,22 +12,18 @@ const PATHS = {
   dist: path.join(__dirname, 'dist')
 };
 
-
 /**
  * Configuration
  */
 module.exports = {
-
   // The base directory where webpack begins
   context: PATHS.src,
-
   entry: {
     // The context property references the source directory and tells
     // webpack to begin there. `main` is just the key that references
     // the starting point of the application, `index.js`
     main: './index.js'
   },
-
   output: {
     // `[name]` will be replaced with the key that references our
     // entry point inside the `entry` object. In this case it will
@@ -36,45 +31,25 @@ module.exports = {
     filename: '[name].bundle.js',
     path: PATHS.dist
   },
-
   module: {
-
     rules: [
       // ES6
       {
         // this regex pattern matches any files with a .js or .jsx
         // file extension
         test: /\.jsx?$/,
-
-        include: [
-          path.join(__dirname, 'src')
-        ],
-
+        include: [ path.join(__dirname, 'src') ],
         // exclude the node_modules folder from being transpiled
-        exclude: [
-          path.join(__dirname, 'node_modules')
-        ],
-
+        exclude: [ path.join(__dirname, 'node_modules') ],
         // transform all .js and .jsx files to standard ES5 syntax
         // using the Babel loader
-        loader: 'babel-loader',
-
-        options: {
-          presets: [
-            [ 'es2015', {"modules": false} ]
-          ]
-        }
+        loader: 'babel-loader'
       }
     ]
   },
-
   plugins: [
     // this plugin generates an index.html file template with
     // our bundled JavaScript injected into the bottom of the body
-    new HtmlWebpackPlugin({
-      title: 'Webpack Demo'
-    })
-
     // this plugin allows for modules that get bundled 2 more more
     // times, defined by the `minChunks` property, in multiple
     // output files to be bundled together in a file called `common.js`.
@@ -86,14 +61,8 @@ module.exports = {
     //   filename: 'commons.js',
     //   minChunks: 2
     // })
-
+    new HtmlWebpackPlugin({ title: 'Webpack Demo' })
   ],
-
   // configuation for the webpack-dev-server plugin
-  devServer: {
-    compress: true,
-    port: 3000,
-    inline: true
-  }
-
-}
+  devServer: { compress: true, port: 3000, inline: true }
+};
