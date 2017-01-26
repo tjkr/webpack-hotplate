20a39afac5499047bbf5b99b36475a84127038bf
Jake Wiesler <jakewiesler@gmail.com>
2017-01-25 20:49:26 -0500

    Clean up ESLint configuration file

--- src/modules/module.js
+++ src/modules/module.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import styles from '../css/module.css';
 
-const Module = () => <h1 className={styles.pink}>Hello from module.js!</h1>;
+const Module = () => <h1 className={styles.pink}>Hellooooo from module.js!</h1>;
 
 export default Module;
